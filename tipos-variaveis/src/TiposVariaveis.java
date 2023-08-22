928cd1faf1ae7b3f358ce74865c2f5c5610edde3
Priscylla Ávila <140451660+priavila@users.noreply.github.com>
2023-08-22 17:27:19 -0300

    estudo operadores e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    //Tipos primitivos
+        byte numero = 10; // não é utilizado
+        short numeroCasa = 315; // não é utilizado 
+
+        // Numeros inteiros
+        int anoNascimento = 1992; // MAIS UTILIZADO 
+
+        long numeroAleatorio = 164165; // não é muito utilizado
+
+        float salario = 10000; // não é muito utilizado
+
+        // Numeros reais
+        double salarioMinimo = 1350; // MAIS UTILIZADO
+
+        // Numeros constantes 
+         final double SALARIO_MINIMO = 2000;
+
+        // Caractere ou texto
+        String meuNome = "Priscylla";
+
+        // Verdadeiro ou Falso
+        boolean variavel = true;
+
     }
 }
