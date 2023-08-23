18bc1644a04b3e306560fc047f44a11456b2548c
Priscylla Ávila <140451660+priavila@users.noreply.github.com>
2023-08-23 15:52:32 -0300

    atualizacao operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,9 +1,21 @@
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
+        String nomeUm = "Priscylla";
+        String nomeDois = new String ("Priscylla");
 
-        System.out.println(numero --);
+        String nomeTres = nomeDois;
 
-        System.out.println(numero);
+        System.out.println(nomeTres == nomeDois);
+      
+      
+        // classe Operadores.java
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        int resultado = a==b ? 1 : 0;
+
+        
     }
 }
